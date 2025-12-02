7e89d83eb98113369ee4312bbac196f4c17c7e63
Norbi <norbert.ujj@gmail.com>
2025-12-02 20:00:25 +0100

    megoldas B orvosolva

--- MintaZH_B_Megoldas/MintaZH_B_Megoldas.ino
+++ MintaZH_B_Megoldas/MintaZH_B_Megoldas.ino
@@ -47,19 +47,23 @@
 
 // --- MEGOLDÁS ---
 
+// C++ code
+//
 #define GLED 3
 #define YLED 4
 #define RLED 5
-#define SENSOR_PIN A1
 
-#define BUFFER_SIZE 5
-int buffer[BUFFER_SIZE]; // Itt tároljuk a hőmérsékletet (egész számként)
+#define SENSOR_PIN A1
+int celsius = 0;
+
+#define BUF_SIZE 5
+int buffer[BUF_SIZE];
 
 // 1. IDŐZÍTŐ LOGIKA
-typedef struct {
+struct Timer_s{
   unsigned long prevTime;
   int deltaTime;
-} Timer_s;
+};
 
 Timer_s BlinkTimer = { .prevTime = 0, .deltaTime = 200 };
 Timer_s MeasureTimer = { .prevTime = 0, .deltaTime = 100 };
@@ -67,7 +71,7 @@ Timer_s MeasureTimer = { .prevTime = 0, .deltaTime = 100 };
 unsigned long hotStateStartTime = 0;
 bool isOverheated = false; 
 
-unsigned char CheckTimer(Timer_s *timer) {
+unsigned char CheckTimer(struct Timer_s *timer) {
   unsigned long actTime = millis();
   if(actTime - timer->prevTime > timer->deltaTime) {
     timer->prevTime = actTime;
@@ -87,48 +91,47 @@ State_e State = ST_COLD;
 
 // 3. SZŰRÉS
 void addData(int data) {
-  for(byte i = 0; i < BUFFER_SIZE-1; i++) {
+  for(byte i = 0; i < BUF_SIZE-1; i++) {
     buffer[i] = buffer[i+1];
   }
-  buffer[BUFFER_SIZE-1] = data;
+  buffer[BUF_SIZE-1] = data;
 }
 
 int getAverage() {
   long sum = 0;
-  for(byte i = 0; i < BUFFER_SIZE; i++) {
+  for(byte i=0; i<BUF_SIZE; i++) {
     sum += buffer[i];
   }
-  return (int)(sum / BUFFER_SIZE);
+  return (int)(sum / BUF_SIZE);
 }
 
-void setup() {
+
+void setup()
+{
+  Serial.begin(9600); 
   pinMode(GLED, OUTPUT);
   pinMode(YLED, OUTPUT);
   pinMode(RLED, OUTPUT);
-  Serial.begin(9600);
 }
 
-void loop() {
-  
+void loop()
+{
   if(CheckTimer(&MeasureTimer)) {
-    int rawAdc = analogRead(SENSOR_PIN);
+    //netrol lopott hogy igy kell csinalni a tinkercades homeronel
+    celsius = map(((analogRead(SENSOR_PIN) - 20) * 3.04), 0, 1023, -40, 125);
+    Serial.print(celsius);
+    Serial.println(" C, ");
     
-    // KONVERZIÓ TMP36-hoz:
-    // 1. ADC -> Feszültség (0-5V)
-    float voltage = rawAdc * (5.0 / 1023.0);
-    // 2. Feszültség -> Celsius fok
-    // 10mV / fok, 500mV offset (0 foknál 0.5V)
-    int tempC = (voltage - 0.5) * 100; 
-
-    addData(tempC);
-    int avgTemp = getAverage();
-
-    Serial.print("Temp: "); Serial.println(avgTemp);
-
+    addData(celsius);
+    int avarage = getAverage();
+    
+    Serial.print("Temp: "); Serial.println(avarage);
+    
+    
     // Állapotváltás logika (°C alapján)
     switch (State) {
       case ST_COLD:
-        if (avgTemp >= 20) {
+        if (avarage >= 20) {
           State = ST_OPTIMAL;
           digitalWrite(YLED, LOW);
           digitalWrite(GLED, HIGH);
@@ -136,12 +139,12 @@ void loop() {
         break;
 
       case ST_OPTIMAL:
-        if (avgTemp < 20) {
+        if (avarage < 20) {
           State = ST_COLD;
           digitalWrite(GLED, LOW);
           digitalWrite(YLED, HIGH);
         } 
-        else if (avgTemp > 30) {
+        else if (avarage > 30) {
           State = ST_HOT;
           digitalWrite(GLED, LOW);
           hotStateStartTime = millis(); 
@@ -150,7 +153,7 @@ void loop() {
         break;
 
       case ST_HOT:
-        if (avgTemp <= 30) {
+        if (avarage <= 30) {
           State = ST_OPTIMAL;
           digitalWrite(RLED, LOW);
           digitalWrite(YLED, LOW);
@@ -159,7 +162,8 @@ void loop() {
         break;
     }
   }
-
+  
+  
   // Állapot viselkedés
   switch (State) {
     case ST_COLD: break;
